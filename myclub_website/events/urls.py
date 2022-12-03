837a7e8b00045d6aea2cc9f2be8a2c0cf1770f0a
devpeak <malaythakur13@gmail.com>
2022-12-03 23:51:00 +0530

    adding calendar to website

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    #int:numbers
+    #str: strings
+    #path: whole urls /
+    #slug: hyphen-  and underscore_
+    #UUID: universally unique identifier
+
+
+    path('<int:year>/<str:month>/', views.home, name="home"),
 ]
--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+import calendar 
+from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request):
-	return render(request, 'home.html', {})
+def home(request,year,month):
+	name = "John"
+	month = month.capitalize() #capitalize the month name
+
+	#convert month from name to number
+	month_number = list(calendar.month_name).index(month)
+	
+	month_number = int(month_number)
+
+	#create a calendar
+	cal = HTMLCalendar().formatmonth(year,month_number)
+
+	#Get curren year
+	now = datetime.now()
+	current_year = now.year
+
+	#Get current time
+	time = now.strftime('%I:%M %p') #I - 11, H- 23 #%H:%M:%S
+
+	return render(request, 
+		'home.html', {
+		"name": name,
+		"year": year,
+		"month": month,
+		"month_number": month_number,
+		"cal":cal,
+		"current_year": current_year,
+		"time":time,
+
+		})
